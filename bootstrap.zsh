40e0938412bcd8c49be954f88a010490bbff2455
kschat <kyleschattler@gmail.com>
2015-04-16 22:06:57 -0400

    Added prompt for failed git installs

--- bootstrap.zsh
+++ bootstrap.zsh
@@ -7,6 +7,18 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 #
+# variable declarations
+#
+
+platform=''
+package_manager=''
+package_manager_arguments=''
+
+log_level=1
+force=false
+default_directory="$HOME"
+
+#
 # configuration variables
 #
 
@@ -32,36 +44,31 @@ osx_dependencies=(
 debian_dependencies=()
 
 git_dependencies=(
-  '--recursive https://github.com/sorin-ionescu/prezto.git ./temp-dir/.zprezto'
-  'https://github.com/gmarik/Vundle.vim.git ./temp-dir/.vim/bundle/Vundle.vim'
+  "--recursive https://github.com/sorin-ionescu/prezto.git $default_directory/.zprezto"
+  "https://github.com/gmarik/Vundle.vim.git $default_directory/.vim/bundle/Vundle.vim"
 )
 
 typeset -A packages
 
+# dotfiles to symlink
+# utility_name -> target_directory
 packages=(
-  git "$HOME"
-  vim "$HOME"
-  terminator "$HOME"
+  git "$default_directory"
+  vim "$default_directory"
+  terminator "$default_directory"
 )
 
 #
-# variable declarations
+# color codes
 #
 
-platform=''
-package_manager=''
-package_manager_arguments=''
+reset_color="$(tput sgr0)"
 
-log_level=2
-force=false
-
-# color codes
-colors=(
-  "$(tput setaf 1)" # error
-  "$(tput setaf 7)" # normal
-  "$(tput setaf 6)" # info
-  "$(tput setaf 3)" # warn
-  "$(tput sgr0)"    # reset
+typeset -A log_colors; log_colors=(
+  error   "$(tput setaf 1)"
+  normal  "$(tput setaf 7)"
+  warn    "$(tput setaf 3)"
+  info    "$(tput setaf 6)"
 )
 
 #
@@ -69,18 +76,19 @@ colors=(
 #
 
 function log {
-  local log_string="${colors[$1]}${2}${reset_color}"
+  local -a levels; levels=("${(@k)log_colors}")
+  local log_string="${log_colors[$1]}${2}${reset_color}"
 
   # test if we should log
-  if [[ "$1" -gt "$log_level" ]]; then
+  if [[ "$1" != 'normal' && "${levels[(i)$1]}" -gt "$log_level" ]]; then
     return 0
   fi
 
   # redirect to stderr if we're logging an error
-  if [[ "$1" -eq 1 ]]; then
-    echo "$log_string" >&2
+  if [[ "$1" == 'error' || "$1" == 'warn' ]]; then
+    echo $log_string >&2
   else
-    echo "$log_string"
+    echo $log_string
   fi
 }
 
@@ -102,9 +110,9 @@ function install_package_manager {
 
 function construct_dependency_arguments {
   case "$1" in
-    yaourt)   echo "--noconfirm -S $dependencies $arch_dependencies";;
-    brew)     echo "install $dependencies $osx_dependencies";;
-    apt-get)  echo "install $dependencies $debian_dependencies";;
+    yaourt)   echo --noconfirm -S $dependencies $arch_dependencies;;
+    brew)     echo install $dependencies $osx_dependencies;;
+    apt-get)  echo install $dependencies $debian_dependencies;;
     *)        return 1;;
   esac
 }
@@ -142,7 +150,7 @@ done
 # detect platform and package manager
 #
 
-log 3 'Detecting platform'
+log info 'Detecting platform'
 
 case "$(uname -rv | tr '[:upper:]' '[:lower:]')" in
   *arch*) platform='arch';;
@@ -151,8 +159,8 @@ case "$(uname -rv | tr '[:upper:]' '[:lower:]')" in
   *) platform='unknown';;
 esac
 
-log 3 "Platform: [$platform]"
-log 3 'Detecting package manager'
+log info "Platform: [$platform]"
+log info 'Detecting package manager'
 
 if hash yaourt 2> /dev/null; then
   package_manager='yaourt'
@@ -164,7 +172,7 @@ else
   package_manager='unknown'
 fi
 
-log 3 "Package manager: [$package_manager]"
+log info "Package manager: [$package_manager]"
 
 if [[ "$platform" == 'unknown' && "$package_manager" == 'unknown' ]]; then
   fatal_error 1 'Unsupported system'
@@ -178,13 +186,20 @@ if [[ "$package_manager" == 'unknown' ]] && ! install_package_manager "$platform
   fatal_error 2 'Failed installing package manager'
 fi
 
+# prompt user before we install dependencies with package manager
+read -q "test?About to install dependencies with $package_manager"$'. Continue? [y/N]\n' || exit 3
+
+#
+# install dependencies with package manager
+#
+
 package_manager_arguments="$(construct_dependency_arguments "$package_manager")"
 
-echo "$package_manager_arguments" | xargs "$package_manager"
+echo $package_manager_arguments | xargs "$package_manager"
 
 # prompt user if installs failed
 if [[ "$?" -ne 0 ]]; then
-  read -q $'continue?No dependencies installed. Continue? [y/N]\n' || exit 3
+  read -q $'continue?An error occured while installing dependencies. Continue? [y/N]\n' || exit 4
 fi
 
 #
@@ -192,11 +207,14 @@ fi
 #
 
 if hash git 2> /dev/null; then
-  mkdir -p ~/.vim/bundle
-
   for dep in $git_dependencies; do
     echo "$dep" | xargs git clone
   done
+fi
+
+# prompt user if installs failed
+if [[ "$?" -ne 0 ]]; then
+  read -q $'continue?An error occured while installing with git. Continue? [y/N]\n' || exit 5
 fi
 
 #
@@ -204,5 +222,10 @@ fi
 #
 
 for package in "${(@k)packages}"; do
-  stow -t "${packages[$package]:-"$HOME"}" "$package"
+  stow -t "${packages[$package]:-"$default_directory"}" "$package"
 done
+
+# prompt user if installs failed
+if [[ "$?" -ne 0 ]]; then
+  read -q $'continue?An error occured while symlinking files.\n'
+fi
